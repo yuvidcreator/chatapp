f477c4caaa49da0cd4a1818736758fe041d7b99e
Yuvraaj Limbole <yuvrajl.103@gmail.com>
2023-11-01 13:28:22 +0530

    asgi.py file updated

--- djangochannel/asgi.py
+++ djangochannel/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-from django.conf.urls import url
+# from django.conf.urls import url
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochannel.settings')
 
 from channels.routing import ProtocolTypeRouter, URLRouter
