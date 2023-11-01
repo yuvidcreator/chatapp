ebb15e5df9305673fa37e26b1db1470d8fde76e5
Yuvraaj Limbole <yuvrajl.103@gmail.com>
2023-11-01 11:01:53 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Texting Chat, Voice and Video calling Test Project @Dataviv 
+- djangochannels and WebRTC
+- django templates
+
+## https://webrtc.org/getting-started/overview
---
+++ user/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+from .forms import LoginForm, UserRegisterForm
+
+def login_page(request):
+    if request.method == 'POST':
+        forms = LoginForm(request.POST)
+        if forms.is_valid():
+            username = forms.cleaned_data['username']
+            password = forms.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('/')
+    else:
+        forms = LoginForm()
+
+    context = {'form': forms}
+    return render(request, 'user/login.html', context)
+
+@login_required
+def logout_page(request):
+    # print('logout')
+    logout(request)
+    return redirect('/user/login')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/user/login')
+
+    else:
+        form = UserRegisterForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'user/register.html',context)
